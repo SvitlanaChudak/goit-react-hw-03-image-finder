6bfda982e6404570a760a6d4feee9c876273e5b8
SvitlanaChudak <svit.lemon@gmail.com>
2023-01-18 16:29:33 +0200

    Add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,3 +14,8 @@ export const App = () => {
     </div>
   );
 };
+// baseURL = 'https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12'
+// apiKey = '31623736-4a8fa2402be59476e61396bec'
+// { id }
+// { webformatURL }
+// { largeImageURL }
